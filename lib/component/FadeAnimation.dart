3f3855eb7e1e3b4153c6cda1b57eaea8a51d43f2
angemissiba <as.missiba@b2i-solutions.com>
2023-04-06 08:14:37 +0200

    #v1

---
+++ lib/component/FadeAnimation.dart
@@ -0,0 +1,42 @@
+import  "package:flutter/material.dart";
+import  "package:simple_animations/simple_animations.dart";
+
+class FadeAnimation extends StatefulWidget {
+  final double delay;
+  final Widget child;
+  const FadeAnimation({Key? key,required this.delay, required this.child }) : super(key: key);
+
+  @override
+  State<FadeAnimation> createState() => _FadeAnimationState();
+}
+
+class _FadeAnimationState extends State<FadeAnimation>  {
+
+
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final tween  =  MultiTrackTween([
+      Track("opacity").add(Duration(milliseconds: 500), Tween(begin: 0.0, end:1.0)),
+      Track("translateY").add(Duration(milliseconds: 500), Tween(begin: -30.0,end :0.0),curve: Curves.easeOut)
+
+    ]);
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (500 * widget.delay).round()),
+      duration:tween.duration,
+      tween: tween,
+      child: widget.child,
+      builderWithChild: (context, child,animation)=>Opacity(
+          opacity: animation["opacity"],
+        child: Transform.translate(
+            offset: Offset(0,animation["translateY"]),
+          child: widget.child,
+        )
+      )
+    );
+  }
+}
